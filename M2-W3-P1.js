9cf516e6ec3ed9227279a27d5029d2e859091de0
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-04-13 17:58:03 +0200

    update on solved question 1

--- M2-W3-P1.js
+++ M2-W3-P1.js
@@ -11,12 +11,12 @@ ASSIGNMENT RULES
 /* EXERCISE 1
  Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
 */
+
+/* WRITE YOUR ANSWER HERE */
 let Area = function (l1, l2){
     return  l1*l2
   }
-
-/* WRITE YOUR ANSWER HERE */
-
+  
 /* EXERCISE 2
  Write a function called "crazySum" which receives two integers as parameters.
  It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
@@ -29,6 +29,7 @@ let crazySum = function (int1, int2) {
         return int1 + int2
    }
 }
+
 
 /* WRITE YOUR ANSWER HERE */
 
